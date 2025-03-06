cac7e6be94a2c35ff4f296a7f9fba76ef0daa752
valevalerio <valerio12bonsignori@gmail.com>
2025-03-06 19:45:32 +0100

    commented the gitignore, added LORE! :D

--- src/cf_generator_lore.py
+++ src/cf_generator_lore.py
@@ -2,28 +2,25 @@
 Handles counterfactual generation for different models using DiCE, LoRE and other methods.
 """
 import os
+import time
 from typing import Dict, Any, List, Union
 import warnings
 import pickle
-from datetime import datetime
-#from dice_ml.utils import helpers
 import pandas as pd
 import numpy as np
 from src.cf_generator_base import CFGeneratorBase
+from src.utils import write_time
 #import wandb
 #############################################
 ################# LORE imports ##############
 #############################################
 from src.LORE_sa.lore_sa import sklearn_classifier_bbox
-
-
 from src.LORE_sa.lore_sa.dataset import TabularDataset
 from src.LORE_sa.lore_sa.encoder_decoder import ColumnTransformerEnc
 from src.LORE_sa.lore_sa.lore import (TabularRandomGeneratorLore,
                                         TabularGeneticGeneratorLore,
                                         TabularRandGenGeneratorLore)
 # from src.LORE_sa.lore_sa.surrogate import DecisionTreeSurrogate
-
 warnings.filterwarnings("ignore",
     message="X has feature names, but StandardScaler was fitted without feature names")
 
@@ -52,6 +49,7 @@ class LoreCFGenerator(CFGeneratorBase):
               reference_data: pd.DataFrame,
               continuous_features: List[str],
               categorical_features: List[str],
+              #ordinal_features: List[str],
               target_name: str = 'target') -> None:
         """
         Setup the counterfactual generator with reference data and feature information.
@@ -62,6 +60,9 @@ class LoreCFGenerator(CFGeneratorBase):
             categorical_features: List of categorical feature names
             target_name: Name of the target column
         """
+        self.target_name = target_name
+        self.continuous_features = continuous_features # are all the features that are not categorical, by default all that are not in the categorical_features nor in the ordinal
+        self.categorical_features = categorical_features
         reference_data[target_name] = reference_data[target_name].astype('category')
         self.dataset = TabularDataset(data=reference_data,
                                       class_name = target_name,
@@ -74,7 +75,8 @@ class LoreCFGenerator(CFGeneratorBase):
         # self.Lore = Lore(bbox=blackbox_model,...)
         # self.generator = TabularRandomGeneratorLore(self.dataset)
 
-    def _setup_model_components(self, model, model_name):
+    def _setup_model_components(self, model, model_name,X_calibration=None,y_calibration=None
+                                )->None:
         """
         Setup the model components for LORE.
         The Lore object and the blackbox model are initialized here.
@@ -110,6 +112,7 @@ class LoreCFGenerator(CFGeneratorBase):
         # Process each model
         for model_name, model in models.items():
             print(f"Generating CFs with with method: ({self._method}) for model: {model_name}")
+            start = time.time()
             # Setup model-specific components if not already done
             self._setup_model_components(model, model_name)
             lore = self._explainers[model_name]
@@ -119,52 +122,71 @@ class LoreCFGenerator(CFGeneratorBase):
             # Generate counterfactuals for each instance
             for idx in range(len(X_calibration)):
                 instance = X_calibration.iloc[idx]
-                print(idx/len(X_calibration))
                 cf_result = lore.explain(instance)
-                print(cf_result)
+                
                 # cf has this form:
                 # {'x': x, 'rule': rule, 'counterfactuals': self.crules, 'deltas': self.deltas}
                 # x is the original instance
                 # rule is the rule that was used to generate the counterfactuals
-                # counterfactuals are the crules and deltas are the distances
+                # counterfactuals are the crules and deltas are the differences with the original rule
+                # counterfactuals_samples are the counterfactuals in the original space that are predicted as different from the original instance
+                # counterfactuals_predictions
 
+                cf_cf_df = pd.DataFrame(cf_result["counterfactual_samples"])
+                cf_cf_df.columns = X_calibration.columns
+                cf_cf_df['target'] = cf_result["counterfactual_predictions"]
                 
                 res_dictionary = {'instance_idx': idx,
                     'original_instance': X_calibration.iloc[idx:idx+1],
                     'true_class': y_calibration.iloc[idx],
-                    'counterfactuals': cf_result["counterfactuals"],
+                    'counterfactuals': cf_cf_df,
                     'rule': cf_result["rule"],
                     'deltas': cf_result["deltas"],
                     'metadata': {
-                        'success':len(cf_result["deltas"])
+                    'success':len(cf_result["counterfactual_samples"])
                     }
                 }
+                # print(len(cf_result["counterfactuals_samples"])/num_cf,"%")
                 rules_and_trees.append({"instance_idx": idx,
-                                      "original_instance": X_calibration.iloc[idx:idx+1],
-                                     "tree": lore.surrogate,
+                                      "tree": lore.surrogate,
                                       "rule": cf_result["rule"]})
                 model_results.append(res_dictionary)
             results[model_name] = model_results
             results_rt[model_name] = rules_and_trees
+            end = time.time()
+            fancy_time = write_time(end-start)
+
+            for idx in range(len(X_calibration)):
+                print("sample",idx,
+                      np.round((model.predict(X_calibration.iloc[[idx]])!=model_results[idx]['counterfactuals']["target"].values)
+                               .sum()/num_cf,2))
+            with open(os.path.join(self.base_path,"time_to_generate_lore.txt"),"a+",encoding='utf-8') as f:
+                f.write(f"{model_name} {fancy_time} {num_cf} {self._method} {set_name} {dt_name} {len(X_calibration)}\n")
+
             # Save the trees, the rules and the counterfactuals
-            # self.save_results(results=model_results,
-            #                   results_rt=rules_and_trees,
-            #                   model_name=model_name,
-            #                   dt_name=dt_name)
+            self.save_results(results=model_results,
+                                model_name= model_name,
+                                dt_name=dt_name,
+                                set_name=set_name)
+            self.save_trees_and_rules(results_rt=rules_and_trees,
+                                        model_name=model_name,
+                                        dt_name=dt_name,
+                                        set_name=set_name)
 
         return results
 
-    def save_results(self, results,results_rt, model_name, dt_name):
+    def save_trees_and_rules(self,results_rt, model_name, dt_name, set_name):
         """
         Save the results of the counterfactual generation.
+        this is specific to LORE, the only method that generates
+        rules and counterfactuals rules as well.
         """
-        # Save the trees
-        save_dir = os.path.join(self.config['paths']['counterfactuals'], model_name, dt_name)
-        # os.makedirs(save_dir, exist_ok=True)
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        filepath = os.path.join(save_dir, f"cf_results_{self._get_method_name()}_{self._method}_{timestamp}_")
-        filepath = filepath + "_rules_and_trees"
-        print(f"Saving results to {filepath}",type(results_rt))
-        print("Saving the others also",type(results))
-        # Save the counterfactuals
-        #super()._save_results(results, model_name, dt_name)
+        # Save the dictionary 
+        filepath = os.path.join(self.base_path,
+                                "rules_and_trees",
+                                f"{model_name}_{dt_name}_rules_and_trees_{set_name}.pkl")
+        # make sure the directory exists
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+        with open(filepath,"wb+") as f:
+            pickle.dump(results_rt,f)
+            print(f"Saved results to {filepath}")
